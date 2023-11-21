c52884c441cc1b86369dc5fb1a9c6b1e2d61dfad
Vlad Y <blakkheart66@gmail.com>
2023-11-21 14:58:41 +0300

    fixes and separation for test workflow and build+deploy workflow

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
         'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'kittygram_password'),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('PORT', '5432'),
     }
 }
@@ -103,7 +103,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
